cf0503453e616d5e85cf77aa31ecb50bff304d0a
SonKatarina23 <houwenlie98@gmail.com>
2020-02-04 12:38:24 +0700

    Configure User Model
    - Override Default User Model
    - Add custom User Manager
    - Create a new super user
    - Set primary key with uuid

--- Playground/Accounts/models.py
+++ Playground/Accounts/models.py
@@ -1,3 +1,52 @@
+# Django Imports
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+# External imports
+from uuid import uuid4
+from phone_field import PhoneField
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not username:
+            raise ValueError('You need to have a valid username')
+        elif not email:
+            raise ValueError('You need to have a valid email address')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username, email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+# Permission mixin gives u is_superuser attribute
+class User(AbstractBaseUser, PermissionsMixin):
+    # Basic Attributes
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Additional Attributes
+    bio = models.CharField(max_length=322)
+    # phone_number = PhoneField(blank=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
