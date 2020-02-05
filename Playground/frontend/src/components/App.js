6bed8c8cb294610003dc56b9cb46e93423001b29
SonKatarina23 <houwenlie98@gmail.com>
2020-02-05 18:32:40 +0700

    ChadAPI, Fix React-Django CORS Bug
    - Connecting DRF End Point to React with Axios
    - Fixing CORS Bug with DRF and React

--- Playground/frontend/src/components/App.js
+++ Playground/frontend/src/components/App.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
+import ChadAPI from "../api/ChadAPI";
 
 export class App extends Component {
+  state = {
+    user: []
+  };
+  test = [
+    { name: "Houwen", state: "Virgin" },
+    { name: "Jess", state: "Virgin" },
+    { name: "Carin", state: "Thotty" },
+    { name: "asdasd", state: "Chad" },
+    { name: "qwe", state: "incel" }
+  ];
+
+  componentDidMount() {
+    ChadAPI.get("User/").then(res => {
+      console.log(res.data);
+      this.setState({ user: res.data });
+    });
+  }
+
+  renderList() {
+    // return (
+    //   <ul>
+    //     {this.test.map(i => {
+    //       return (
+    //         <li>
+    //           {i.name} is a {i.state}
+    //         </li>
+    //       );
+    //     })}
+    //   </ul>
+    if (this.state.user === []) return <div>Loading . . .</div>;
+    else {
+      return (
+        <ul>
+          {this.state.user.map(i => {
+            return <li key={i.id}>{i.username}</li>;
+          })}
+        </ul>
+      );
+    }
+  }
   render() {
-    return <div>App</div>;
+    return <div>{this.renderList()}</div>;
   }
 }
 
