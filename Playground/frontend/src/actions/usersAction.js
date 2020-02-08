b971918b275f81d7c5935cedebd89fe20c3ab13e
SonKatarina23 <houwenlie98@gmail.com>
2020-02-08 09:05:55 +0700

    Refactor Follow / Unfollow

--- Playground/frontend/src/actions/usersAction.js
+++ Playground/frontend/src/actions/usersAction.js
@@ -25,25 +25,21 @@ export const fetchSingleUser = id => async (dispatch, getState) => {
   }
 };
 
+// NASTY STUFF BRO
 export const toggleFollowing = id => async (dispatch, getState) => {
   const currentUserID = getState().auth.currentUser.id;
   const users = getState().users;
   const targetUser = users.find(user => user.id === id);
   let tempData;
-  if (targetUser.followers.find(followerID => followerID === currentUserID)) {
-    console.log("we are currently following, trying to unfollow");
-    tempData = {
-      followers: targetUser.followers.filter(
-        followerID => followerID !== currentUserID
-      )
-    };
-  } else {
-    console.log("we are currently NOT following, trying to follow");
-    tempData = {
-      followers: [...targetUser.followers, currentUserID]
-    };
-  }
 
+  const isFollowing = targetUser.followers.find(
+    followerID => followerID === currentUserID
+  );
+  tempData = {
+    followers: isFollowing
+      ? targetUser.followers.filter(followerID => followerID !== currentUserID)
+      : [...targetUser.followers, currentUserID]
+  };
   try {
     const res = await ChadAPI.patch(`User/${id}/`, tempData);
     dispatch({
